8a91cd6bc603983de855e6590d452b4885ef8851
Centny <centny@gmail.com>
2014-09-23 01:56:19 +0800

    adding example.

--- jnigo_test.go
+++ jnigo_test.go
@@ -13,6 +13,7 @@ import (
 // 	fmt.Println("end...")
 // }
 func init() {
+	initjava()
 	Init("-Djava.class.path=java/bin")
 }
 func initjava() {
@@ -426,21 +427,34 @@ func TestString(t *testing.T) {
 }
 
 func TestAshow(t *testing.T) {
-	vm := &GVM
-	clsa := vm.FindClass("Ljnigo/A;")
+	clsa := GVM.FindClass("Ljnigo/A;")
 	obja, _ := clsa.New()
 	objv, _ := obja.As("Ljava/lang/Object;")
-	objs, _ := vm.NewAry("Ljava/lang/Object;", 1)
+	objs, _ := GVM.NewAry("Ljava/lang/Object;", 1)
 	objs.SetObject(0, objv)
 	fmt.Println(obja.CallVoid("show", objv))
 	fmt.Println(obja.CallVoid("show", objs))
 	show, err := obja.CallObject("show", "Ljava/lang/String;",
-		true, Byte(1), Char(1), int16(1),
-		1, int64(1), float32(1), float64(1),
-		objv, "jjjjj", []bool{false}, []Byte{1, 2},
-		[]Char{3, 4}, []int16{11, 12}, []int{21, 22},
-		[]int64{31, 32}, []float32{41, 42}, []float64{51, 52},
-		objs, []string{"aaa", "bbb"},
+		true,              //for java boolean
+		Byte(1),           //for java byte
+		Char(1),           //for java char
+		int16(1),          //for java short
+		1,                 //for java int
+		int64(1),          //for java long
+		float32(1),        //for java float
+		float64(1),        //for java double
+		objv,              //for java Object
+		"jjjjj",           //for java String
+		[]bool{false},     //for java boolean[]
+		[]Byte{1, 2},      //for java byte[]
+		[]Char{3, 4},      //for java char[]
+		[]int16{11, 12},   //for java short[]
+		[]int{21, 22},     //for java int[]
+		[]int64{31, 32},   //for java long[]
+		[]float32{41, 42}, //for java float[]
+		[]float64{51, 52}, //for java double[]
+		objs,              //for java Object[]
+		[]string{"aaa"},   //for java String[]
 	)
 	if err != nil {
 		t.Error(err.Error())
@@ -448,3 +462,19 @@ func TestAshow(t *testing.T) {
 	}
 	fmt.Println(show.ToString())
 }
+
+// func TestExample(t *testing.T) {
+// 	// Init("-Djava.class.path=java/bin")
+// 	str_a, err := GVM.New("java.lang.String")
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// 	fmt.Println(str_a.CallInt("length"))
+// 	//
+// 	str_b, err := GVM.New("java.lang.String", "abc")
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// 	fmt.Println(str_b.CallInt("length"))
+// 	fmt.Println(str_b.AsString())
+// }
